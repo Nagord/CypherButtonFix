d0159be1b2a7a0dabd3438c62c2623bdcbd4cb44
DragonFire47 <1redyoung@gmail.com>
2020-11-04 01:58:17 -0500

    Add project files.

---
+++ CypherButtonFix/Patch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using static PulsarPluginLoader.Patches.HarmonyHelpers;
+
+namespace CypherButtonFix
+{
+    [HarmonyPatch(typeof(PLSylvassiCypher_ControlStation), "Update")]
+    class ControlStationUpdatePatch
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> targetSequence = new List<CodeInstruction>()
+            {
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(UnityEngine.Color), "get_magenta")),
+            };
+
+            List<CodeInstruction> injectedSequence = new List<CodeInstruction>()
+            {
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(UnityEngine.Color), "get_yellow")),
+            };
+            return PatchBySequence(instructions, targetSequence, injectedSequence, PatchMode.REPLACE);
+        }
+    }
+}
---
+++ CypherButtonFix/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using PulsarPluginLoader;
+
+namespace CypherButtonFix
+{
+    public class Plugin : PulsarPlugin
+    {
+        public override string Version => "1.0.0";
+
+        public override string Author => "Dragon, Engbot";
+
+        public override string LongDescription => "text";
+
+        public override string Name => "CypherButtonFix";
+
+        public override string HarmonyIdentifier()
+        {
+            return "Dragon.CypherButtonFix";
+        }
+    }
+}
